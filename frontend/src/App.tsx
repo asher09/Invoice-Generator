337542ae0059e5937f162537fdcf90c36ba260b3
Asher <sharmaaman0202@gmail.com>
2025-07-17 02:15:39 +0530

    frontend init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="bg-[#141414] w-full h-screen items-center">
+          <div className="flex justify-between w-full bg-[#1f1f1f] py-2.5 px-15">
+            <div> Levitation </div>
+            <div>
+              <button type="button" className="text-black bg-[#CCF575] cursor-pointer hover:bg-[#1f1f1f] hover:border-1  hover:text-[#CCF575] font-medium rounded-lg 
+              text-sm px-5.5 py-3 text-center me-2">Login</button>
+            </div>
+          </div>
+          <div>
+
+          </div>
+      </div>
+    </>
+  )
+}
+
+export default App
