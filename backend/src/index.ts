36ba71caa4ba508fd9975e36e1806191136abaf1
Asher <sharmaaman0202@gmail.com>
2025-07-20 02:05:56 +0530

    mongodb connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,12 +11,7 @@ process.env.DEBUG = '';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: process.env.NODE_ENV === 'production'
-    ?['https://levitation-invgen.vercel.app']
-    : ['http://localhost:5173'],
-    credentials: true
-})) 
+app.use(cors()) 
 app.use('/api/user',  userRouter);
 app.use('/api/invoice', invoiceRouter);
 
